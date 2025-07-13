e180e4e810893d0f0874387d508870c96618b126
wedyzza <ggwp.ezpezlmon@mail.ru>
2025-07-13 20:04:52 +0500

    basic models

--- kaleidoskop/api/models.py
+++ kaleidoskop/api/models.py
@@ -1,3 +1,50 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    pass
+
+
+class Item(models.Model):
+    pass
+
+
+class Order(models.Model):
+    pass
+
+
+class Like(models.Model):
+    pass
+
+
+class Cart(models.Model):
+    pass
+
+
+class CartItem(models.Model):
+    pass
+
+
+class Banner(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
+
+
+class CommentReply(models.Model):
+    pass
+
+
+class Document(models.Model):
+    pass
+
+
+class ItemMedia(models.Model):
+    pass
+
+
+class Media(models.Model):
+    pass
+
