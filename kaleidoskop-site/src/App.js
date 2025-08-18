8900d747d09c3b849b4fa14015040331b85a7d34
polya <polyakrupina@gmail.com>
2025-08-18 16:28:39 +0500

    create header

---
+++ kaleidoskop-site/src/App.js
@@ -0,0 +1,13 @@
+import './styles/reset.scss'
+import './styles/common.scss'
+import MainPage from './pages/MainPage/MainPage';
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage />
+    </div>
+  );
+}
+
+export default App;
---
+++ kaleidoskop-site/src/pages/MainPage/MainPage.jsx
@@ -0,0 +1,12 @@
+import Header from '../../components/Header/Header';
+import './mainPage.scss'
+
+function MainPage () {
+    return (
+        <div className="page">
+            <Header />
+        </div>
+    )
+}
+
+export default MainPage;
