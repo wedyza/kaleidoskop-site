9745296f251601334f10be86f1f9fbc3ba035103
polya <polyakrupina@gmail.com>
2025-08-19 10:29:58 +0500

    update header styles

--- kaleidoskop-site/src/pages/MainPage/MainPage.jsx
+++ kaleidoskop-site/src/pages/MainPage/MainPage.jsx
@@ -2,11 +2,11 @@ import Header from '../../components/Header/Header';
 import './mainPage.scss'
 
 function MainPage () {
-    return (
-        <div className="page">
-            <Header />
-        </div>
-    )
+  return (
+    <div className="page">
+      <Header />
+    </div>
+  )
 }
 
 export default MainPage;
