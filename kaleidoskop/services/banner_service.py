cc57ed141302e2dc2a9f749b6c023a88baaef486
wedyza <wedyza@mail.ru>
2026-03-13 14:26:30 +0500

    migration

--- kaleidoskop/services/banner_service.py
+++ kaleidoskop/services/banner_service.py
@@ -9,7 +9,10 @@ class BannerService:
         return self._banner_repository.get_public_queryset(group_type)
     
     def get_max_queue(self, group_type: Banner.BannerGroupType) -> int:
-        return self._banner_repository.get_max_queue().queue
+        q = self._banner_repository.get_max_queue(group_type)
+        if q is None:
+            return 0
+        return q.queue
     
     def get_admin_queryset(self, group_type: Banner.BannerGroupType) -> Iterable[Banner]:
         return Banner.objects.filter(group_type=group_type).order_by('queue').all()
